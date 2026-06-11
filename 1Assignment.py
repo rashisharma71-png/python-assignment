2885df1781f8dfbb8ccc8a2fda1d9a71eff9fa64
rashisharma71-png <rashisharm71@gmail.com>
2026-06-11 21:54:02 +0530

    assignment1

---
+++ 1Assignment.py
@@ -0,0 +1,13 @@
+#with multiple object
+#with seperator
+# with "ends"
+
+
+print("red","yellow","blue","pink",sep="   ")
+print("hello maya",end="  ")
+
+
+
+#2 question
+name=input("enter your name")
+print("hello", name +"welcome")
